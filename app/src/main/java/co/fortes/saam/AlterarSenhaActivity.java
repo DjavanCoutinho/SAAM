8e5f4d957a9401710c2ebf45798fa55399945116
Engeziellithon vieira <zievs32@yahoo.com.br>
2018-03-13 20:56:52 -0300

    telas saam

---
+++ app/src/main/java/co/fortes/saam/AlterarSenhaActivity.java
@@ -0,0 +1,13 @@
+package co.fortes.saam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlterarSenhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_senha);
+    }
+}
