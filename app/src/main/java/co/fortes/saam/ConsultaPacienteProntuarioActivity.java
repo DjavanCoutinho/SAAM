1b75be4bdb9c31b72797bb70482c135723433284
Engeziellithon vieirazievs <zievs32@yahoo.com.brz>
2018-04-01 18:28:54 -0300

    telas novas

--- app/src/main/java/co/fortes/saam/ConsultaPacienteProntuarioActivity.java
+++ app/src/main/java/co/fortes/saam/ConsultaPacienteProntuarioActivity.java
@@ -3,11 +3,11 @@ package co.fortes.saam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CadastroEnderecoActivity extends AppCompatActivity {
+public class ConsultaPacienteProntuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_endereco);
+        setContentView(R.layout.activity_consulta_paciente_prontuario);
     }
 }
---
+++ app/src/main/java/co/fortes/saam/ConsultaPacientoFotoExamesActivity.java
@@ -0,0 +1,13 @@
+package co.fortes.saam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsultaPacientoFotoExamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_paciento_foto_exames);
+    }
+}
